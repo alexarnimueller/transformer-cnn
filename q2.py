c8e9061b16a4bf598e3bdf8d7b690f04866bcc4a
Alex Müller <alex.mueller@pharma.ethz.ch>
2020-03-31 09:01:38 +0200

    refactoring

--- q2.py
+++ q2.py
@@ -1,27 +1,25 @@
-
 import sys
 import math
 import numpy as np
 
-fp = open(sys.argv[1], "r");
+fp = open(sys.argv[1], "r")
 
-p = [];
-y = [];
+p = []
+y = []
 
 for line in fp:
-    line = line.strip()[:-1].split("\t");
-    y.append(float(line[0]));
-    p.append(float(line[1]));
+    line = line.strip()[:-1].split("\t")
+    y.append(float(line[0]))
+    p.append(float(line[1]))
 
-r2 = np.corrcoef(p, y)[0, 1];
-r2 = r2 * r2;
+r2 = np.corrcoef(p, y)[0, 1]
+r2 = r2 * r2
 
-press = 0.0;
+press = 0.0
 for i in range(len(y)):
-   press += ( p[i] - y[i] ) * ( p[i] - y[i] );
+    press += (p[i] - y[i]) * (p[i] - y[i])
 
-rmsep = math.sqrt(press / len(y));
+rmsep = math.sqrt(press / len(y))
 
-print("q2 = ", r2);
-print("RMSE (cv) = ", rmsep);
-
+print("q2 = ", r2)
+print("RMSE (cv) = ", rmsep)
